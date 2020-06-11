06639bc87aa5bc680ff0128fbdca361125700eff
Louis Brauer <louis77@mac.com>
2020-06-11 21:47:50 +0200

    Switch to png icons, update icon cache on install

--- meson/post_install.py
+++ meson/post_install.py
@@ -5,7 +5,10 @@ import subprocess
 
 install_prefix = os.environ['MESON_INSTALL_PREFIX']
 schemadir = os.path.join(install_prefix, 'share/glib-2.0/schemas')
+datadir = os.path.join(install_prefix, 'share')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling the gsettings schema ...')
     subprocess.call(['glib-compile-schemas', schemadir])
+    print('Updating icon cache…')
+    subprocess.call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
